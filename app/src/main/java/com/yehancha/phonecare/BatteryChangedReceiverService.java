901639479ed4daf4489f4593e825e07be25d8ee8
Yehan Pemarathne <yehancha@gmail.com>
2015-07-16 13:59:36 +0530

    Removed unwanted Toasts

--- app/src/main/java/com/yehancha/phonecare/BatteryChangedReceiverService.java
+++ app/src/main/java/com/yehancha/phonecare/BatteryChangedReceiverService.java
@@ -33,8 +33,6 @@ public class BatteryChangedReceiverService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "BatteryNotifiationService starting", Toast.LENGTH_SHORT).show();
-
         return START_STICKY;    // we don't have an intent to deliver
     }
 
